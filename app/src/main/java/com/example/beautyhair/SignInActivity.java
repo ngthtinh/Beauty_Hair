bc60ec14adfd41fdfd7ba55c8797603db8c20e38
ngthtinh <nguyenthanhtinh365@gmail.com>
2021-07-21 17:27:33 +0700

    Sign in 90% done!

--- app/src/main/java/com/example/beautyhair/SignInActivity.java
+++ app/src/main/java/com/example/beautyhair/SignInActivity.java
@@ -1,14 +1,78 @@
 package com.example.beautyhair;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.beautyhair.data.model.Account;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
 
 public class SignInActivity extends AppCompatActivity {
+    EditText editTextPhone, editTextPassword;
+    Button buttonSignIn;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        textView = findViewById(R.id.textView2);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_account = database.getReference("Accounts");
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                // ProgressDialog signInDialog = new ProgressDialog(SignInActivity.this);
+                // signInDialog.setMessage("Please wait...");
+                // signInDialog.show();
+
+                table_account.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                        // signInDialog.dismiss();
+                        textView.setText(editTextPhone.getText().toString());
+                        if (snapshot.child(editTextPhone.getText().toString()).exists()) {
+                            Account account = snapshot.child(editTextPhone.getText().toString()).getValue(Account.class);
+                            if (account.getPassword().equals(editTextPassword.getText().toString())) {
+                                Toast.makeText(SignInActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                                Intent mainActivity = new Intent(SignInActivity.this, MainActivity.class);
+                                startActivity(mainActivity);
+                            } else {
+                                Toast.makeText(SignInActivity.this, "Đăng nhập thất bại!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            Toast.makeText(SignInActivity.this, "Tài khoản không tồn tại!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/beautyhair/data/model/Account.java
+++ app/src/main/java/com/example/beautyhair/data/model/Account.java
@@ -1,2 +1,32 @@
-package com.example.beautyhair.data.model;public class Account {
+package com.example.beautyhair.data.model;
+
+public class Account {
+    private String Name;
+    private String Password;
+
+    public Account()
+    {
+    }
+
+    public Account(String name, String password)
+    {
+        Name = name;
+        Password = password;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
